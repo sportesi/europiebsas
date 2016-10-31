d2ef25708a71c795915db0a8b5824f310a211b48
María Belén Sciegata <mbsciegata@gmail.com>
2016-10-31 20:09:05 -0300

    Saco slider viejo

--- inc_script.php
+++ inc_script.php
@@ -4,18 +4,4 @@
 	<script src="js/bootstrap.min.js"></script>
     <script src="js/mailer.js" type="text/javascript"></script>
     <script src="bower_components/bootstrap-sweetalert/dist/sweetalert.min.js" type="text/javascript"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-    <script src="js/responsiveslides.min.js"></script>
-    <script>
-    $(function () {
-      $("#slider1").responsiveSlides({
-        auto: false,
-        pager: true,
-        nav: true,
-        speed: 500,
-        maxwidth: 800,
-        namespace: "centered-btns"
-      });
-    });
-  </script>
 <!--/Script-->
