08fe6c8e1203472a3c3d480235516f6720e4cf5f
Sebastian Portesi <sebastian@proyectiva.com>
2016-11-02 12:08:54 -0300

    Logica para activar item del menu terminada

--- inc_header.php
+++ inc_header.php
@@ -1,41 +1,41 @@
 <div class="container">
-	<header>
-		<nav class="navbar navbar-default">
-			<div class="container-fluid">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-1">
-						<span class="sr-only">Menu</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a href="index.php" class="navbar-brand">
-						<img src="img/logo-header.png"/>
-					</a>
-				</div>
-				<div class="collapse navbar-collapse" id="navbar-1">
-					<ul class="nav navbar-nav">
-						<li class="active">
-							<a href="index.php">Home</a>
-						</li>
-						<li>
-							<a href="nosotros.php">Sobre nosotros</a>
-						</li>
-						<li>
-							<a href="plantillas.php">Plantillas</a>
-						</li>
-						<!--<li>
-							<a href="novedades.php">Novedades</a>
-						</li>-->
-						<li>
-							<a href="centros.php">Centros</a>
-						</li>
-						<li class="link-contacto">
-							<a href="contacto.php">Contacto</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-	</header>
+    <header>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-1">
+                        <span class="sr-only">Menu</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a href="index.php" class="navbar-brand">
+                        <img src="img/logo-header.png"/>
+                    </a>
+                </div>
+                <div class="collapse navbar-collapse" id="navbar-1">
+                    <ul class="nav navbar-nav">
+                        <li>
+                            <a href="index.php">Home</a>
+                        </li>
+                        <li>
+                            <a href="nosotros.php">Sobre nosotros</a>
+                        </li>
+                        <li>
+                            <a href="plantillas.php">Plantillas</a>
+                        </li>
+                        <!--<li>
+                                <a href="novedades.php">Novedades</a>
+                        </li>-->
+                        <li>
+                            <a href="centros.php">Centros</a>
+                        </li>
+                        <li class="link-contacto">
+                            <a href="contacto.php">Contacto</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
 </div>
--- inc_script.php
+++ inc_script.php
@@ -1,9 +1,19 @@
 <!--Script-->
-	<script src="http://code.jquery.com/jquery-latest.js"></script>
-	<script src="js/jquery.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-    <script src="js/mailer.js" type="text/javascript"></script>
-    <script src="bower_components/bootstrap-sweetalert/dist/sweetalert.min.js" type="text/javascript"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-	<script src="js/slider.min.js"></script>
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/mailer.js" type="text/javascript"></script>
+<script src="bower_components/bootstrap-sweetalert/dist/sweetalert.min.js" type="text/javascript"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="js/slider.min.js"></script>
 <!--/Script-->
+
+<!--Activar item del menu-->
+<script type="text/javascript">
+    $(document).ready(function () {
+        var path = window.location.pathname.split("/");
+        var file = path[path.length - 1];
+        $(".nav").find("a[href='" + file + "']").parent('li').addClass('active');
+    });
+</script>
+<!-- End Activar item del menu-->
